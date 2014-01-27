475d67ee71b19188c5bcf7d8e503899475d8f3d4
janardan <railsexception@yahoo.com>
2014-01-26 22:52:24 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TemplApp::Application.config.secret_token = 'c9719c7684bdfe5c4ece9515e677789d82180d0c275dc77f8c17c5a11b72529242293d7f2fe03cdc76ae6ef82fc9fc6c3458522eee1df48c5e30047ba24d9b71'
